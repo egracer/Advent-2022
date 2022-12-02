18dc624c59ab661525db5588fb419ca642f9a3d9
Ethan Gracer <4086884+egracer@users.noreply.github.com>
2022-12-02 22:58:22 +0000

    Switch to functional approach & add second solution

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+from functools import reduce
+
 def parse_input():
-  with open('test_input.txt') as f:
+  with open('full_input.txt') as f:
     lines = f.read().split('\n\n')
     for i, line in enumerate(lines):
       lines[i] = line.split()
     
     return lines
 
-def stringsToInts(array):
-  out = []
-  for s in array:
-    out.append(int(s))
-  return sum(out)
+def getSubArraySum(array):
+  return reduce(lambda sum, item: sum + int(item), array, 0)
+
+def solutionOne(input):
+  return max(input)
+
+def solutionTwo(input):
+  return sum(list(sorted(input, reverse=True))[0:3])
 
 def main():
-  input = parse_input()
-  print(max(map(stringsToInts, input)))
+  input = list(map(getSubArraySum, parse_input()))
+  print("1:", solutionOne(input))
+  print("2:", solutionTwo(input))
 
 if __name__ == "__main__":
     main()
